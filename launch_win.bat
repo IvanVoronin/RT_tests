669fd9083bc73f59e43db63675269ffd01463712
IvanVoronin <prostojean@gmail.com>
2018-11-17 23:47:29 +0300

    Fixing bat file

--- launch_win.bat
+++ launch_win.bat
@@ -1,4 +1,9 @@
 @ECHO off
 TITLE Launch the RT test battery
+set gitdir="../Progs/git"
+set path=%gitdir%/cmd;%path%
+git checkout RT_tests_45min_2018
+git pull
 python.exe launch_win.py
+rem Uncomment the next line if you have more than one Python distro installed
 rem "C:\Program Files (x86)\PsychoPy2\python.exe" launch.py
